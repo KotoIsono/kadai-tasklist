a7a52a38cdd84a72b961cedc8871d7aa41bb3013
koto.isono <0eu145507870d9w@gmail.com>
2021-01-25 19:23:22 +0900

    a

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static final String PERSISTENCE_UNIT_NAME = "tasks";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
@@ -13,7 +13,7 @@ public class DBUtil {
     }
 
     private static EntityManagerFactory getEntityManagerFactory() {
-        if (emf == null) {
+        if(emf == null) {
             emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
 
